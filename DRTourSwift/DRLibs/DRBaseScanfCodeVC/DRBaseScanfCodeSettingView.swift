33ab6a4359e00e29ca4219a1ee956d7993b647b3
Daredos <asiosldh@163.com>
2016-07-22 00:42:46 +0800

    添加扫描动画

--- DRTourSwift/DRLibs/DRBaseScanfCodeVC/DRBaseScanfCodeSettingView.swift
+++ DRTourSwift/DRLibs/DRBaseScanfCodeVC/DRBaseScanfCodeSettingView.swift
@@ -8,8 +8,22 @@
 
 import UIKit
 
-class DRBaseScanfCodeSettingView: UIView {
+let w = UIScreen.mainScreen().bounds.size.width;
+let h = UIScreen.mainScreen().bounds.size.width;
 
+
+class DRBaseScanfCodeSettingView: UIView {
+    
+    var linView: UIImageView?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib();
+        self.linView = UIImageView.init(frame: CGRectMake((w-200)/2.0, (h-64-200)/2.0, 200, 2));
+        self.linView?.backgroundColor = UIColor.redColor();
+        self.addSubview(self.linView!);
+        self.linView?.layer.addAnimation(self.getAnimation(), forKey:nil);
+    }
+    
     internal class func baseScanfCodeSettingViewWithFrame(frame: CGRect) -> (DRBaseScanfCodeSettingView) {
         let baseScanfCodeSettingView = NSBundle.mainBundle().loadNibNamed("DRBaseScanfCodeSettingView", owner: nil, options: nil).first as! DRBaseScanfCodeSettingView;
         baseScanfCodeSettingView.autoresizingMask = .None;
@@ -17,4 +31,14 @@ class DRBaseScanfCodeSettingView: UIView {
         return baseScanfCodeSettingView;
     }
     
+    func getAnimation() -> (CABasicAnimation) {
+
+        let animation = CABasicAnimation.init(keyPath: "position.x");
+        animation.duration = 1.3333;
+        animation.fromValue = "\((h-64-200)/2.0)";
+        animation.toValue   = "\((h-64-200)/2.0+200)";
+        animation.repeatCount = MAXFLOAT;
+        animation.autoreverses = true;
+       return animation;
+    }
 }
